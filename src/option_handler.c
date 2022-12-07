4af211acffb52daf174638076a1bf252edf869c1
nbarnes <nick.a.barnes44@gmail.com>
2022-12-07 10:30:50 -0500

    update usage and help

--- src/option_handler.c
+++ src/option_handler.c
@@ -9,12 +9,11 @@ void usage(char *program_name)
     }
     // Usage
     printf("\n\033[1mUSAGE\033[m\n");
-    printf("    %s [-c <project_path>] [-p <project_path>] [-n <program_name(s)...>]\n", program_name);
+    printf("    %s -c <project_path> [-n <program_name(s)...>]\n", program_name);
 
     // Options
     printf("\n\033[1mOPTIONS\033[m\n");
     printf("    \033[1m-c\033[m     generate a C project\n");
-    printf("    \033[1m-p\033[m     generate a Python project\n");
     printf("    \033[1m-n\033[m     specify a program name(s) for your project\n");
     printf("\n    For a full listing of options and detailed descriptions, use %s --help.\n", program_name);
 
@@ -38,34 +37,32 @@ void help(char *program_name)
     }
     // Summary
     printf("\n\033[1mSUMMARY\033[m\n");
-    printf("    %s - Project file generator for C and Python\n", program_name);
+    printf("    %s - Project file generator for C\n", program_name);
 
     // Description
     printf("\n\033[1mDESCRIPTION\033[m\n");
     printf("    %s is a tool which automatically generates default project\n", program_name);
-    printf("    directories and files for C or Python projects at a\n");
-    printf("    user-specified project path.\n");
+    printf("    directories and files for C or projects at a user-specified\n");
+    printf("    project path.\n");
     printf("\n    -Files generated for C projects loosely follow the BARR-C standard\n");
-    printf("    -Files generated for Python projects are PEP8 compliant.\n");
     printf("\n    For more information on files and file structure see the %s(1)\n", program_name);
     printf("    manpage (\"man %s\").\n", program_name);
 
     // Usage
     printf("\n\033[1mUSAGE\033[m\n");
-    printf("    %s [-c <project_path>] [-p <project_path>] [-n <program_name(s)...>]\n", program_name);
+    printf("    %s -c <project_path> [-n <program_name(s)...>]\n", program_name);
 
     // Options
     printf("\n\033[1mOPTIONS\033[m\n");
     printf("    \033[1m-c\033[m     generate a C project\n");
-    printf("    \033[1m-p\033[m     generate a Python project\n");
     printf("    \033[1m-n\033[m     specify a program name(s) for your project\n");
 
     // Examples
     printf("\n\033[1mEXAMPLES\033[m\n");
     printf("    Example 1 (C):\n");
     printf("        $ ./%s -c /home/user/project -n encode decode\n", program_name);
-    printf("\n    Example 2 (Python):\n");
-    printf("        $ ./%s -p /home/user/project\n", program_name);
+    printf("\n    Example 2 (C):\n");
+    printf("        $ ./%s -c /home/user/project\n", program_name);
 
 
 END:
@@ -100,10 +97,19 @@ exit_code_t process_options(int argc, char **argv, options_t *options)
         goto END;
     }
 
-    int option = 0;                 // Option used for getopt
     char program_name[] = "pk";
 
-    while ((option = getopt(argc, argv, "c:p:n:l:tah")) != -1)
+    if (argc < 2)
+    {
+        exit_code = E_OUT_OF_BOUNDS;
+        printf("Must have at least one option -c <project_path>\n");
+        usage(program_name);
+        goto END;
+    }
+
+    int option = 0;                 // Option used for getopt
+
+    while ((option = getopt(argc, argv, "c:n:h")) != -1)
     {
         switch (option)
         {
@@ -118,17 +124,10 @@ exit_code_t process_options(int argc, char **argv, options_t *options)
                     goto END;
                 }
 
-                // Ignore a p_flag if passed
-                if (true == options->p_flag)
-                {
-                    break;
-                }
-
                 // Check if the directory exists
                 exit_code = directory_exists(optarg);
                 if (exit_code != E_DIRECTORY_EXISTS)
                 {
-                    printf("here\n");
                     print_exit_message(exit_code);
                     goto END;
                 }
@@ -138,30 +137,10 @@ exit_code_t process_options(int argc, char **argv, options_t *options)
                 options->c_flag = true;
                 break;
             
-            case 'p':
-
-                // Check if the p_flag was previously turned on
-                if (true == options->p_flag)
-                {
-                    usage(program_name);
-                    exit_code = E_INVALID_INPUT;
-                    print_exit_message(exit_code);
-                    goto END;
-                }
-
-                // Ignore a c_flag if passed
-                if (true == options->c_flag)
-                {
-                    break;
-                }
-
-                options->p_flag = true;
-                break;
-            
             case 'n':
 
                 // Check if the n_flag was previously turned on
-                if (true == options->n_flag)
+                if ((true == options->n_flag) || (false == options->c_flag))
                 {
                     usage(program_name);
                     exit_code = E_INVALID_INPUT;
@@ -185,25 +164,7 @@ exit_code_t process_options(int argc, char **argv, options_t *options)
 
                     options->prog_names[idx] = argv[optind];
                     options->num_prog_names += 1;
-                    //DEBUG
-                    printf("option %c with arg '%s' = num_prog_name[%ld]\n", option, argv[optind], options->num_prog_names);
-                    //DEBUG
                     idx++;
-                    optind++;
-                }
-                break;
-            
-            case 'l':
-                optind--;
-                while (optind < argc-1)
-                {
-                    if (NULL != strstr(argv[optind], "-"))
-                    {
-                        break;
-                    }
-
-                    //printf("option %c with arg '%s'\n", option, argv[optind]);
-                    //printf("%s\n", options->prog_names[idx]);
                     optind++;
                 }
                 break;
